7a99b8971a8a44d47cba1132f885998b0fa198bc
changzhisheng <changzhisheng@daojia-inc.com>
2018-12-17 20:12:18 +0800

    增加工具类

---
+++ src/main/java/com/czs/practice/springlearn/tools/MyIOUtils.java
@@ -0,0 +1,16 @@
+package com.czs.practice.springlearn.tools;
+
+public class MyIOUtils {
+
+
+    //使用springboot启动的时候，注释掉这个main方法
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+
+}
---
+++ src/main/java/com/czs/practice/springlearn/tools/MyNioUtils.java
@@ -0,0 +1,26 @@
+package com.czs.practice.springlearn.tools;
+
+/**
+ * NIO 工具类
+ */
+public class MyNioUtils {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 读取 文件
+     */
+    public static Object readFromFile(Object file){
+
+
+
+        return null;
+    }
+
+
+
+
+}
